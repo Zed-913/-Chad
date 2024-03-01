a20ee3100f0bfad392f57ef8df679dcbed1b71ff
Zed-913 <z.djebbar@esi-sba.dz>
2024-03-01 15:49:42 +0100

    Creation

---
+++ src/App.tsx
@@ -0,0 +1,181 @@
+import { useState } from 'react'
+import logo from './assets/1.jpeg'
+import './App.css'
+import logo2 from './assets/3.jpeg'
+
+function App() {
+  return (
+    <div className="container">
+      <div className="navb">
+            <h1 className="logs">GIGA<span className="logspan">CHAD</span></h1>
+
+      <div className="Nav">
+
+      <h1 className="A">RoadMaP</h1>
+      <h1 className="B">About</h1>
+      <h1 className="C">LAUNCH</h1>
+      <h1 className="D">TOKEN</h1>
+      </div>
+      </div>
+
+
+    <div className="main">
+          <img src={logo} alt="CHAD" className="chad"/>
+      
+
+          <p className="gigamsg">
+
+            <span>$CHAD</span><br/>
+            
+            
+            
+            <br/>Dear Crypto Enthusiast,
+
+Time to wake up and work for your bags. 💰<br/>
+
+You know the drill. Buy low, sell high. Hodl strong. Don't let the FUD get to you. 💪<br/>
+
+You are part of a revolution. A new era of decentralized finance. You are not a sheep. You are a lion. 🦁<br/>
+
+You have the vision. The courage. The conviction. You see the potential of blockchain technology.<br/> You understand the power of smart contracts. You believe in the future of crypto. 🚀<br/>
+
+You are not alone. You have a community. A tribe. A family. You have me. Giga Chad. Your mentor. Your friend. Your leader. 😎
+<br/>
+I am here to guide you. To inspire you. To motivate you. To challenge you. To make you the best version of yourself. 🔥<br/>
+
+Together, we will conquer the crypto space. We will dominate the market. We will create wealth and freedom for ourselves and others. We will change the world. 🌎<br/>
+
+Are you ready? Let's go. 🏃‍♂️<br/>
+
+Sincerely,<br/>
+Giga Chad<br/>
+</p>
+      </div>
+
+
+
+<div className="scroll">
+
+<div className="left">
+<p className="gigamsg2">
+
+<span>$CHAD</span><br/>
+
+
+
+<br/>Dear Crypto Enthusiast,
+
+Time to wake up and work for your bags. 💰<br/>
+
+You know the drill. Buy low, sell high. Hodl strong. Don't let the FUD get to you. 💪<br/>
+
+You are part of a revolution. A new era of decentralized finance. You are not a sheep. You are a lion. 🦁<br/>
+
+You have the vision. The courage. The conviction. You see the potential of blockchain technology.<br/> You understand the power of smart contracts. You believe in the future of crypto. 🚀<br/>
+
+You are not alone. You have a community. A tribe. A family. You have me. Giga Chad. Your mentor. Your friend. Your leader. 😎
+<br/>
+I am here to guide you. To inspire you. To motivate you. To challenge you. To make you the best version of yourself. 🔥<br/>
+
+Together, we will conquer the crypto space. We will dominate the market. We will create wealth and freedom for ourselves and others. We will change the world. 🌎<br/>
+
+Are you ready? Let's go. 🏃‍♂️<br/>
+
+Sincerely,<br/>
+Giga Chad<br/>
+</p>
+
+</div>
+
+
+
+<div className="right">
+<img src={logo2} alt="CHAD2" className="chad2" />
+
+</div>
+
+
+</div>
+<div className="third">
+  <div className="title">
+  <h1>RoadMaP</h1>
+  <p className='descr'>The Milly Plan</p>
+  </div>
+  
+  <div className="timeline">
+
+
+    <div className="checkpoint">
+      <div className='tr'>
+          <h2>Phase 01 : <span className='SUUU'>Launch</span></h2>
+          <ul>
+              <li>Token Launch</li>
+              <li>LP Burn</li>
+              <li>500 Holders</li>
+              <li>Start marketing compaigns</li>
+          </ul>
+
+
+
+      </div>
+
+
+
+    </div>
+
+
+    <div className="checkpoint">
+      <div className='tr2'>
+          <h2>Phase 02 : <span className='SUUU'>Raiding</span></h2>
+          <ul>
+              <li>1000 Holders</li>
+              <li>Social Media competitions & rewards</li>
+              <li>Launch of NFT Sale</li>
+              <li>Blast Ecosystem Collaboration</li>
+          </ul>
+
+
+
+      </div>
+
+
+
+    </div>
+
+
+
+    <div className="checkpoint">
+      <div className='tr'>
+          <h2>Phase 03 : <span className='SUUU'>$CHAD</span> in The MOON</h2>
+          <ul>
+              <li>Social media expansion</li>
+              <li>NFT mints out</li>
+              <li>Ecosystem Collaboration</li>
+              <li>Staking/LP rewards activation</li>
+              <li>Coinmarketcap Listing</li>
+              <li>Coingecko Listing</li>
+          </ul>
+
+
+
+      </div>
+
+
+
+    </div>
+
+
+  </div>
+
+
+</div>    
+
+
+  </div>
+  )
+}
+
+
+
+
+export default App
